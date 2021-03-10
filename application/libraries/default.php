788fbd7f847c8e772222e87483b2ed2dcfb6fada
Renato Nascimento <renatoalves_nascimento@hotmail.com>
2021-03-10 00:53:02 -0300

    Codeigniter install

---
+++ application/libraries/default.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class NPS_niveis
+{
+    private $CI;
+
+    public function __construct()
+    {
+        $this->CI = &get_instance();
+    }
+}
---
+++ application/views/errors/html/error_404.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>NPS Cruzeiro do Sul</title>
+	<link rel="icon" href="https://npscruzeirodosuleducacional.com/assets/images/favicon.png">
+	<link rel="apple-touch-icon" href="https://npscruzeirodosuleducacional.com/assets/images/favicon.png">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<style type="text/css">
+		body {
+			background-image: url('https://npscruzeirodosuleducacional.com/assets/images/tela_fundo.png');
+		}
+
+		p {
+			padding: 0;
+			margin: 0;
+		}
+	</style>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-136971474-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-136971474-1');
+	</script>
+
+</head>
+
+<body>
+	<div class="container">
+		<div class="row justify-content-center my-5">
+			<div class="col-12 col-md-6 text-center">
+				<h1 style="color: white">Página não encontrada</h1>
+			</div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col-12 col-md-6">
+				<img src="https://npscruzeirodosuleducacional.com/assets/images/logos2.png" class="img-fluid">
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
