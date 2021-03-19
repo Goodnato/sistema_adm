abcb43f684076913bc54b50c9aa3534d7548bdbb
Cleyton Carvalho <cleytonn.carvalhoo@gmail.com>
2021-03-19 14:15:17 -0300

    inclusão dos model no controller e divisão das views

--- application/controllers/Linhas.php
+++ application/controllers/Linhas.php
@@ -7,10 +7,26 @@ class Linhas extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('Categorias_model');
     }
 
     public function index()
     {
+        $this->load->view('head', $this->carregaView());
         $this->load->view('linhas/index');
+        $this->load->view('footer');
     }
+
+    private function carregaView()
+    {        
+        $carregaView = [
+            'caminhoCss' => 'assets/css/linhas.css',
+            'caminhoJs' => 'assets/js/linhas.js',
+            'listaCategorias' => $this->Categorias_model->consultaTodasCategorias(),
+        ];
+
+        return $carregaView;
+    }
+
+
 }
