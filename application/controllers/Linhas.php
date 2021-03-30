06bb5418253a209b5d55fc0ce814c007dfdc6810
Cleyton Carvalho <cleytonn.carvalhoo@gmail.com>
2021-03-30 18:52:39 -0300

    comecei a inclusão do metodo salvar linha

--- application/controllers/Linhas.php
+++ application/controllers/Linhas.php
@@ -29,5 +29,16 @@ class Linhas extends CI_Controller
         return $carregaView;
     }
 
+    public function salvarLinha(){
+        $this->form_validation->set_rules("imei", "<b>Imei</b>", "trim|required|integer|is_unique[aparelhos.imei1]|exact_length[15]");
+        $this->form_validation->set_rules("idModelo", "<b>Modelo</b>", "trim|required|integer|combines[modelos.id]");
+        $this->form_validation->set_rules("idStatusCondicaoAparelho", "<b>Condição aparelho</b>", "trim|required|integer|combines[status_condicoes_aparelhos.id]");
+        $this->form_validation->set_rules("notaFiscal", "<b>Nota fiscal</b>", "trim|integer|max_length[50]");
+        $this->form_validation->set_rules("dataNotaFiscal", "<b>Data nota fiscal</b>", "trim|valid_date[Y-m-d]");
+        $this->form_validation->set_rules("valorNotaFiscal", "<b>Valor nota fiscal</b>", "trim|decimal");
+
+        
+    }
+
 
 }
