219ab4b40ac2b5e79dd09dcecc1289a52ccaef43
Cleyton Carvalho <cleytonn.carvalhoo@gmail.com>
2021-03-31 13:58:58 -0300

    Alterações no controler linha

--- application/controllers/Linhas.php
+++ application/controllers/Linhas.php
@@ -30,12 +30,35 @@ class Linhas extends CI_Controller
     }
 
     public function salvarLinha(){
-        $this->form_validation->set_rules("imei", "<b>Imei</b>", "trim|required|integer|is_unique[aparelhos.imei1]|exact_length[15]");
-        $this->form_validation->set_rules("idModelo", "<b>Modelo</b>", "trim|required|integer|combines[modelos.id]");
-        $this->form_validation->set_rules("idStatusCondicaoAparelho", "<b>Condição aparelho</b>", "trim|required|integer|combines[status_condicoes_aparelhos.id]");
-        $this->form_validation->set_rules("notaFiscal", "<b>Nota fiscal</b>", "trim|integer|max_length[50]");
-        $this->form_validation->set_rules("dataNotaFiscal", "<b>Data nota fiscal</b>", "trim|valid_date[Y-m-d]");
-        $this->form_validation->set_rules("valorNotaFiscal", "<b>Valor nota fiscal</b>", "trim|decimal");
+        $this->form_validation->set_rules("numeroLinha", "<b>Número da Linha</b>", "trim|required|integer|is_unique[linhas.numero_linha]|exact_length[10]");
+        $this->form_validation->set_rules("codigoChip", "<b>Código do Chip</b>", "trim|required|integer|is_unique[linhas.codigo_chip]|exact_length[15]");
+        $this->form_validation->set_rules("idCategoria", "<b>Categoria</b>", "trim|required|integer|combines[categorias.id]");
+        $this->form_validation->set_rules("operadora", "<b>Operadora</b>", "trim|max_length[50]");
+        $this->form_validation->set_rules("pinPuk1", "<b>Pin-Puk1</b>", "trim|integer|max_length[12]");
+        $this->form_validation->set_rules("pinPuk2", "<b>Pin-Puk2</b>", "trim|integer|max_length[12]");
+
+        if (!$this->form_validation->run()) {
+            echo json_encode([
+                'status' => false,
+                'mensagem' => validation_errors()
+            ]);
+            
+            return false;
+        }
+
+        $this->Linhas_model->salvarLinha([
+            'numero_linha' => $this->input->post('numeroLinha'),
+            'codigo_chip' => $this->input->post('codigoChip'),
+            'id_categoria' => $this->input->post('idCategoria'),
+            'operadora' => $this->input->post('operadora'),
+            'pin_puk1' => $this->input->post('pinPuk1'),
+            'pin_puk2' => $this->input->post('pinPuk2'),
+            'id_usuario_registro' => 1
+        ]);
+
+        echo json_encode([
+            'status' => true
+        ]);
 
         
     }
