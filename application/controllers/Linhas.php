23572babe85adfac9694536ac55d8680b93ef24a
Cleyton Carvalho <cleytonn.carvalhoo@gmail.com>
2021-04-01 22:15:37 -0300

    Alterações no metodo salvarLinha

--- application/controllers/Linhas.php
+++ application/controllers/Linhas.php
@@ -7,7 +7,9 @@ class Linhas extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('Linhas_model');
         $this->load->model('Categorias_model');
+        $this->load->model('Operadoras_model');
     }
 
     public function index()
@@ -23,17 +25,19 @@ class Linhas extends CI_Controller
             'caminhoCss' => 'assets/css/linhas.css',
             'caminhoJs' => 'assets/js/linhas.js',
             'listaCategorias' => $this->Categorias_model->consultaTodasCategorias(),
-            'listaCategoriasAtivas' => $this->Categorias_model->consultaTodasCategoriasPorStatus(STATUS_ATIVO)
+            'listaCategoriasAtivas' => $this->Categorias_model->consultaTodasCategoriasPorStatus(STATUS_ATIVO),
+            'listaOperadoras' => $this->Operadoras_model->consultaTodasOperadoras(),
+            'listaOperadorasAtivas' => $this->Operadoras_model->consultaTodasOperadorasPorStatus(STATUS_ATIVO)
         ];
 
         return $carregaView;
     }
 
     public function salvarLinha(){
-        $this->form_validation->set_rules("numeroLinha", "<b>Número da Linha</b>", "trim|required|integer|is_unique[linhas.numero_linha]|exact_length[10]");
+        $this->form_validation->set_rules("numeroLinha", "<b>Número da Linha</b>", "trim|required|integer|is_unique[linhas.numero_linha]|exact_length[11]");
         $this->form_validation->set_rules("codigoChip", "<b>Código do Chip</b>", "trim|required|integer|is_unique[linhas.codigo_chip]|exact_length[15]");
         $this->form_validation->set_rules("idCategoria", "<b>Categoria</b>", "trim|required|integer|combines[categorias.id]");
-        $this->form_validation->set_rules("operadora", "<b>Operadora</b>", "trim|max_length[50]");
+        $this->form_validation->set_rules("idOperadora", "<b>Operadora</b>", "trim|required|integer|combines[operadoras.id]");
         $this->form_validation->set_rules("pinPuk1", "<b>Pin-Puk1</b>", "trim|integer|max_length[12]");
         $this->form_validation->set_rules("pinPuk2", "<b>Pin-Puk2</b>", "trim|integer|max_length[12]");
 
@@ -50,7 +54,7 @@ class Linhas extends CI_Controller
             'numero_linha' => $this->input->post('numeroLinha'),
             'codigo_chip' => $this->input->post('codigoChip'),
             'id_categoria' => $this->input->post('idCategoria'),
-            'operadora' => $this->input->post('operadora'),
+            'id_operadora' => $this->input->post('idOperadora'),
             'pin_puk1' => $this->input->post('pinPuk1'),
             'pin_puk2' => $this->input->post('pinPuk2'),
             'id_usuario_registro' => 1
